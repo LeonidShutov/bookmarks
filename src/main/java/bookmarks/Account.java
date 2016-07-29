e28a4486d01675433d7468181f00063ceeaba448
LeonidShutov <shizopotam@gmail.com>
2016-07-29 13:16:41 +0300

    repositories implementation

---
+++ src/main/java/bookmarks/Account.java
@@ -0,0 +1,50 @@
+package bookmarks;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Account {
+
+    @OneToMany(mappedBy = "account")
+    private Set<Bookmark> bookmarks = new HashSet<>();
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @JsonIgnore
+    private String password;
+
+    private String username;
+
+    public Account() {
+    }
+
+    public Account(String password, String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public Set<Bookmark> getBookmarks() {
+        return bookmarks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
